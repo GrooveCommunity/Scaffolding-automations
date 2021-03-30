055e938db5e406bc6e4929282162dc455bbf9c2d
codermarcos <coder.marcos@gmail.com>
2021-03-30 12:50:29 -0300

    :art: improve typescript

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-export async function handler(event, context) {
-    console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-    return context.logStreamName
-  }
+import { APIGatewayProxyHandler } from 'aws-lambda';
+
+export const handler: APIGatewayProxyHandler = async (event, context) => {
+  console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+  
+  return {
+    statusCode: 200,
+    body: 'DONE',
+  };
+}
